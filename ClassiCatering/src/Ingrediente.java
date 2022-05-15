e64a8b9fd7709c5647b5abc3d74a30b5c1917a9e
Edoardo Galtieri <93085719+Edoardo644@users.noreply.github.com>
2022-05-15 17:27:43 +0200

    inizio
    inizializzato il progetto springBoot e creato le classi principali in un altro progetto normale perché non so dove metterle  nel progetto spring poi lo capirò

---
+++ ClassiCatering/src/Ingrediente.java
@@ -0,0 +1,28 @@
+
+public class Ingrediente {
+	
+	private String nome;
+	private String origine;
+	private String descrizione;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getOrigine() {
+		return origine;
+	}
+	public void setOrigine(String origine) {
+		this.origine = origine;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	
+
+}
---
+++ ClassiCatering/src/Piatto.java
@@ -0,0 +1,42 @@
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+public class Piatto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	private List<Ingrediente> ingredientiDelPiatto;
+	
+	private String nome;
+	private String descr;
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+
+}
